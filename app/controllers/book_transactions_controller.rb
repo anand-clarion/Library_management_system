4ff0a1a2f753698f75e0c2055cfb5761887549b6
Anand Tripathi <anand.tripathi@clariontechnologies.co.in>
2014-08-26 17:55:42 +0530

    Added basic crud functionality for book transaction model

--- app/controllers/book_transactions_controller.rb
+++ app/controllers/book_transactions_controller.rb
@@ -1,19 +1,47 @@
 class BookTransactionsController < ApplicationController
+
+  # This action show all book_transactions
   def index
+    @book_transactions = BookTransaction.all
   end
 
+  # This action created a new book_transaction instance  
   def new
+    @book_transaction = BookTransaction.new
   end
 
+  # This actions added a new record in book_transaction table
   def create
+    @book_transaction = BookTransaction.new(book_transaction_params)
+    @book_transaction.issue_date = Time.now
+    if @book_transaction.save
+      flash[:notice] = "Transaction saved successfully"
+      redirect_to book_transactions_url
+    else
+      render "new"
+    end
   end
 
+  # This action edit a book_transaction's inforamation
   def edit
+    @book_transaction = BookTransaction.find(params[:id])
   end
 
+  # This action update a book_transaction's information
   def update
+    @book_transaction = BookTransaction.find(params[:id])
+    if @book_transaction.update_attributes(return_date: Time.now)
+      flash[:notice] = "Transaction information successfully updated"
+      redirect_to book_transactions_url
+    else
+      render "edit"
+    end
   end
 
-  def destroy
+  # This action permit all accessible attributes
+  def book_transaction_params
+    params.require(:book_transaction).permit(:student_id, :book_id, :issue_date, :return_date, :fine)
   end
+
 end
+
